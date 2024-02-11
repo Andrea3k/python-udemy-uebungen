d9ea2b74749054e69dd1428fc35d0aa7fb31c9f0
Andrea Schmidl <andrea-schmidl@web.de>
2024-02-11 16:03:28 +0100

    overview

---
+++ Day2_data_types.py
@@ -0,0 +1,21 @@
+# data types
+
+# string 
+# die Nummer in den eckigen Klammer gibt den Buchstaben an der Stelle aus; -1 ist ist das erste Zeichen von hinten
+print("Hello"[-1])
+print("Hello"[0])
+
+
+#integer 
+
+# die unterstriche Teilen die Zahl auf zur besseren Lesbarkeit - werden nicht ausgegeben
+123_456_789
+
+#float (floating point number) 
+
+3.14159
+
+# bolean - großgeschrieben 
+
+True 
+False 
