c8eb4ad2174fc5d0ea5ad70409da7d52a3d93b0c
Andrea Schmidl <andrea-schmidl@web.de>
2024-02-18 13:50:54 +0100

    done

---
+++ Day3_if_else.py
@@ -0,0 +1,9 @@
+# Rollercoaster height check
+# <= oder >= higher/lower or equal to 
+# != not equal to
+# == equal to --> Vergleicht den Wert und speichtert nicht in den Wert die Variable
+height = int(input("What is your hight in cm?\n"))
+if height >= 120: 
+    print("You can ride the rollercoaster!")
+else: 
+    print("Sorry, you are out!")
