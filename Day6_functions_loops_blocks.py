567866e94b4010ffe44633a066a4258c22c21b0b
Andrea Schmidl <andrea-schmidl@web.de>
2024-03-09 18:20:58 +0100

    v1

---
+++ Day6_functions_loops_blocks.py
@@ -0,0 +1,18 @@
+# parenthesis --> function vgl. len("Hello"); print(num)
+# functions start with def and end with : 
+# in the functions there are specified steps/instructions
+
+# define the function with the def key word
+# give the function a name 
+# (): 
+
+def my_function(): 
+    print("Hello")
+    print("Bye")
+
+# call the function - otherwise nothing happens
+my_function()
+
+# while-loop --> runs as long as the statement is true
+while something_is_true: 
+    # Do something repeatedly
