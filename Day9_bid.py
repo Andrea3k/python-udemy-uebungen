26166f25a84cedf57e95fb237e382857cbf4d119
Andrea Schmidl <andrea-schmidl@web.de>
2024-03-22 18:33:31 +0100

    v1

---
+++ Day9_bid.py
@@ -0,0 +1,47 @@
+# ask for name input
+# ask for bid price
+# add name and bid in dictionary as key and value
+# ask if there are other users who want to bid
+# clear screen
+# find the highest bid in the dictionary and declare them as the winner
+
+import os 
+
+name = input("What is your name?: ")
+bid = abs(int(input("What is your bid?: ")))
+further_user = input("Are there further users?: ") # yes or no
+
+if further_user == "yes": 
+    futher_user = True
+elif further_user == "no": 
+    further_user = False
+
+
+def add_name_bid (name, bid)
+    dict_name_bid = {}
+    dict_name_bid.append({
+        name: bid
+    })
+    return dict_name_bid
+
+dict_name_bid = add_name_bid(name, bid)
+
+current_bid = 0
+def highest_bid(): 
+    for n in dict_name_bid: 
+        if current_bid < dict_name_bid[n]
+            n = n + 1 
+        elif current_bid == dict_name_bid[n]
+
+        else: 
+        return ()
+
+
+
+for users in dict_name_bid: 
+    if further_user == True: 
+        dict_name_bid = add_name_bid(name, bid)
+        os.system('cls')
+    else:
+        highest_bid()
+
