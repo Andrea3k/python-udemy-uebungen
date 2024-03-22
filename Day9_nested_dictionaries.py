02c46fdbdd73c2882b1eaf32cebcda5f0c3a228c
Andrea Schmidl <andrea-schmidl@web.de>
2024-03-22 17:40:09 +0100

    v1

---
+++ Day9_nested_dictionaries.py
@@ -0,0 +1,56 @@
+# nesting a list in a dictionary
+# nesting a dictionary in a list
+# nesting a dictionary in a dictionary
+
+
+# travel_log = {
+#     "France": ["Paris", "Lille", "Dijon"]
+#     "Germany": ["Berlin", "Hamburg", "Stuttgart"]
+# }
+
+
+
+# travel_log2 = {
+#     "France": {
+#                 "cities_visited": ["Nice", "Bordeaux", "Paris"], 
+#                "cities_to_visit": ["Strassbourg", "Marseille", "Champagne"],
+#                }
+#     "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+# }
+
+# travel_log2["France"]["cities_to_visit"]
+
+
+country = input("Country name: ") # Add country name
+visits = int(input("Visits: ")) # Number of visits
+list_of_cities = eval(input("List of cities: ")) # create list from formatted string
+
+travel_log = [
+  {
+    "country": "France",
+    "visits": 12,
+    "cities": ["Paris", "Lille", "Dijon"]
+  },
+  {
+    "country": "Germany",
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Stuttgart"]
+  },
+    
+]
+# Do NOT change the code above 👆
+
+# TODO: Write the function that will allow new countries
+# to be added to the travel_log. 
+
+def add_new_country(country, visits, list_of_cities):  
+    travel_log.append({
+            "country": country, 
+            "visits": visits, 
+            "cities": list_of_cities,
+        })
+
+# Do not change the code below 👇
+add_new_country(country, visits, list_of_cities)
+print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+print(f"My favourite city was {travel_log[2]['cities'][0]}.")
